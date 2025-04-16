f82cb633a01f274b73c599319424aaffd9b2c0fa
juliaalmeida13 <julia.almeida@estudante.ufscar.br>
2025-04-16 00:38:09 -0300

    fix: reduced time to 1 sec and modified default brower to msedge

--- igprint/config.py
+++ igprint/config.py
@@ -1,22 +1,25 @@
 import os
 import configparser
 
+
 def get_config():
     # Define o caminho para o arquivo de configuração no diretório home do usuário
     config_path = os.path.expanduser("~/.igprint_config.ini")
     config = configparser.ConfigParser()
-    
+
+    user = os.getlogin()
+
     # Se o arquivo existir, lê as configurações; se não, cria com valores padrões
     if os.path.exists(config_path):
         config.read(config_path)
     else:
         # Cria um arquivo de configuração com valores padrão
         config['DEFAULT'] = {
-            'chrome_path': r'C:/Program Files/Google/Chrome/Application/chrome.exe',
-            'user_data_dir': r'C:/Users/<user>/AppData/Local/Google/Chrome/User Data'
+            'edge_path': r'C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe',
+            'user_data_dir': f'C:/Users/{user}/AppData/Local/Microsoft/Edge/User Data',
         }
         with open(config_path, 'w') as configfile:
             config.write(configfile)
-    
+
     # Retorna os valores de configuração
-    return config['DEFAULT']['chrome_path'], config['DEFAULT']['user_data_dir']
+    return config['DEFAULT']['edge_path'], config['DEFAULT']['user_data_dir']
